1303f3120aa2a2a5efab426a9f7b66d71701682d
jmpnz <dumpsterdev@gmail.com>
2022-04-03 02:24:59 +0000

    refactor(elfdump):break switch case body to functions

--- json.go
+++ json.go
@@ -6,48 +6,56 @@ import (
 	"strings"
 )
 
+func dumpELF32(p *Parser, jsonOutput strings.Builder) (string, error) {
+	jsonBin, err := json.MarshalIndent(p.F.ELFBin32, "", "  ")
+	if err != nil {
+		return "", err
+	}
+	jsonSymbols, err := json.MarshalIndent(p.F.ELFSymbols, "", " ")
+	if err != nil {
+		return "", err
+	}
+	_, err = jsonOutput.Write(jsonBin)
+	if err != nil {
+		return "", err
+	}
+	_, err = jsonOutput.Write(jsonSymbols)
+	if err != nil {
+		return "", err
+	}
+	return jsonOutput.String(), nil
+
+}
+
+func dumpELF64(p *Parser, jsonOutput strings.Builder) (string, error) {
+	jsonBin, err := json.MarshalIndent(p.F.ELFBin64, "", "  ")
+	if err != nil {
+		return "", err
+	}
+	jsonSymbols, err := json.MarshalIndent(p.F.ELFSymbols, "", " ")
+	if err != nil {
+		return "", err
+	}
+	_, err = jsonOutput.Write(jsonBin)
+	if err != nil {
+		return "", err
+	}
+	_, err = jsonOutput.Write(jsonSymbols)
+	if err != nil {
+		return "", err
+	}
+	return jsonOutput.String(), nil
+}
+
 // DumpRawJSON marshals the raw binary representation into JSON Format.
 func (p *Parser) DumpRawJSON() (string, error) {
-
 	var jsonOutput strings.Builder
 
 	switch p.F.Class() {
 	case ELFCLASS32:
-		jsonBin, err := json.MarshalIndent(p.F.ELFBin32, "", "  ")
-		if err != nil {
-			return "", err
-		}
-		jsonSymbols, err := json.MarshalIndent(p.F.ELFSymbols, "", " ")
-		if err != nil {
-			return "", err
-		}
-		_, err = jsonOutput.Write(jsonBin)
-		if err != nil {
-			return "", err
-		}
-		_, err = jsonOutput.Write(jsonSymbols)
-		if err != nil {
-			return "", err
-		}
-		return jsonOutput.String(), nil
+		return dumpELF32(p, jsonOutput)
 	case ELFCLASS64:
-		jsonBin, err := json.MarshalIndent(p.F.ELFBin64, "", "  ")
-		if err != nil {
-			return "", err
-		}
-		jsonSymbols, err := json.MarshalIndent(p.F.ELFSymbols, "", " ")
-		if err != nil {
-			return "", err
-		}
-		_, err = jsonOutput.Write(jsonBin)
-		if err != nil {
-			return "", err
-		}
-		_, err = jsonOutput.Write(jsonSymbols)
-		if err != nil {
-			return "", err
-		}
-		return jsonOutput.String(), nil
+		return dumpELF64(p, jsonOutput)
 	default:
 		return "", errors.New("unsupported ELF Class")
 	}
